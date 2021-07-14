2c6d7ef09a5e100615795e4acbbf4e8da8a65d8d
Lucas Giori Cesconetto <lucas.giori@gazin.com.br>
2021-07-14 01:21:14 -0300

    test: Implemetando teste aplicando a inversão de dependências, informando a data para qual precisar ser calcula taxas e totais,

--- src/OrderTest.php
+++ src/OrderTest.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class OrderTest extends TestCase
@@ -25,9 +26,21 @@ class OrderTest extends TestCase
         $order->addItem(new Beer(description: "Itaipava",price: 5));
         $order->addItem(new Water(description: "Crystal 300ml",price: 2));
 
-        $taxes = $order->getTaxes();
+        $taxes = $order->getTaxes(date: new DateTime(datetime: "2021-07-09"));
 
         $this->assertEquals(expected: 2.5,actual: $taxes);
+    }
+
+    public function testOrderCalculateHolidayTaxes(): void
+    {
+        $order = new Order();
+        $order->addItem(new Cigar(description: "marlboro",price: 10));
+        $order->addItem(new Beer(description: "Itaipava",price: 5));
+        $order->addItem(new Water(description: "Crystal 300ml",price: 2));
+
+        $taxes = $order->getTaxes(date: new DateTime(datetime: "2021-01-09"));
+
+        $this->assertEquals(expected: 1.5,actual: $taxes);
     }
 
     public function testOrderCalculateTotal(): void
@@ -38,7 +51,7 @@ class OrderTest extends TestCase
         $order->addItem(new Water(description: "Crystal 300ml",price: 2));
         $order->addItem(new Eletronic(description: "Celular J5",price: 10));
 
-        $total = $order->getTotal();
+        $total = $order->getTotal(date: new DateTime());
 
         $this->assertEquals(expected:34.5,actual: $total);
     }
